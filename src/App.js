e14f538318f8a754d1a9c9e276cc06a59a1d23b0
shivatheja3 <74763721+shivatheja3@users.noreply.github.com>
2022-09-28 10:32:49 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom';
+
+function App() {
+  const element =
+    (
+      <div >
+        <h1>Welcome to CBIT</h1>
+        <h1>Now the Time is</h1>
+        <p>{new Date().toLocaleTimeString()}</p>
+      </div>
+    );
+  ReactDOM.render(element, document.getElementById('root'));
+}
+setInterval(App, 1000);
+
+export default App;
